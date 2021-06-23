b067caa86f740fb6bf3005bfa781543f24629f12
Kheyyon <parktwins1@gmail.com>
2021-06-23 11:08:28 -0400

    IT Book API had CORS errors so I had to use primary API. I got input values up and running. Now I need to hook all functions back up in foreach loop.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 //Global Variables
-const api_url = 'https://api.itbook.store/1.0/search/'
+const api_url = 'https://api.itbook.store/1.0/search'
 const input = document.querySelector('#blank')
 const text = document.querySelector('#search')
 let h2 = document.querySelector('h2')
 const searchResults = []
 const books = document.querySelector('.book-list')
 
-
 function storeText() {
-  console.log(input.value)
-  getBooks(input)
-  return input
+  let words = input.value
+  let searchQuery = words.split('').join('+')
+  console.log(searchQuery)
+  getBooks(words)
+  //return input
 }
 
 text.addEventListener('click', storeText)
@@ -18,10 +19,10 @@ text.addEventListener('click', storeText)
 
 async function getBooks(input) {
   try {
-    const response = await axios.get(`https://api.itbook.store/1.0/search/${input}`) 
-    console.log(response.data.books)
+    const response = await axios.get(`http://openlibrary.org/search.json?q=${input}`) 
+    console.log(response)
 //Goal here is to add multiple [key, value] pairs onto on 1 html object
-    makeBooks(response.data.books)
+   makeBooks(response.data.books)
     
   } catch (error) {
    console.error(error) 
@@ -35,16 +36,19 @@ function makeBooks(bookData) {
     // console.log(element.subtitle)
     // console.log(element.image)
    
+    //Book Appending Features for text and innerText cintent
     let newBook = document.createElement('div')
     newBook.className = "book"
     let bookContainer = document.createElement('div')
-    bookContainer.className = "container"
+    bookContainer.className = "class1"
     newBook.append(bookContainer)
     let bookContent = document.createElement('p')
-    bookContent.className = "bkContent show-description"
+    bookContent.className = "bkContent class1"
     bookContent.textContent =
     `Book Title:${element.title}, Subheading: ${element.subtitle}`
     books.append(newBook)
+    
+    //Image Appending Features (can split into another function to improve aesthetic of code)
     let imgDiv = document.createElement('div')
     imgDiv.className = "book cover"
     let newImg = document.createElement('img')
@@ -53,17 +57,18 @@ function makeBooks(bookData) {
     imgDiv.append(newImg)
     bookContainer.append(imgDiv)
     imgDiv.append(bookContent)
+
   });
 }
 
 
-function toggleContent() {
-  let showText = document.querySelector('.book');
-  showText.style.display === "none" ? showText.style.display = "block" : showText.style.display = "none";
+function toggleContent(element) { 
+ element.className === "class1" ? element.className = "class2" : element.className = "class1"
 }
 
-books.addEventListener('click',toggleContent())
+// books.addEventListener('click', toggleContent(class1))
 
-
+//Toggle Visibility 
 //https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 //dash.generalassemb.ly - lesson 3 
+//https://www.youtube.com/watch?v=degwrcZeGF0
