d97bb7d73958daefcf10b563d4426ad88d452af1
Kheyyon <parktwins1@gmail.com>
2021-06-23 15:02:13 -0400

    I debugged my program again... I am going to add a modal that activates when the button is clicked.

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const books = document.querySelector('.book-list')
 function storeText() {
   let words = input.value
   let searchQuery = words.split('').join('+')
+  //source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+  //source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+  //source: https://masteringjs.io/tutorials/fundamentals/string-concat - this gave me the idea to check MDN for examples
   console.log(searchQuery)
   getBooks(words)
   //return input
@@ -22,8 +25,8 @@ async function getBooks(input) {
     const response = await axios.get(`http://openlibrary.org/search.json?q=${input}`) 
     let bookInfo = response.data.docs  
 //Goal here is to add multiple [key, value] pairs onto on 1 html object
-   makeBooks(bookInfo.slice(0,20))
-    
+    makeBooks(bookInfo.slice(0, 20))
+    //Source: Molly Exten - slack - sorting function 
   } catch (error) {
    console.error(error) 
   }
@@ -40,31 +43,41 @@ function makeBooks(bookData) {
     let newBook = document.createElement('div')
     newBook.className = "book"
     let bookContainer = document.createElement('div')
-    bookContainer.className = "class1"
+    bookContainer.className = "modal"
+    bookContainer.setAttribute('id','myModal')
     newBook.append(bookContainer)
+    
+    //create a span element for modal
+    let modalSpan = document.createElement('span')
+    modalSpan.className = "close"
+    
+
+    //Make and set book content 
     let bookContent = document.createElement('p')
-    bookContent.className = "bkContent class1"
-    bookContent.textContent =
-    `Book Title:${element.title} Author: ${element.author_name} Publisher: ${element.publisher}`
+    bookContent.className = "bookContent"
+    bookContent.textContent =`Book Title:${element.title} Author: ${element.author_name} Publisher: ${element.publisher}`
+    modalSpan.append(bookContent)
+
+    //Features to append button to books and add innerText to them
+    let newBookBtn = document.createElement('button')
+    newBookBtn.setAttribute('id', 'myBtn')
+    newBookBtn.textContent = "details"
+    bookContent.append(newBookBtn)
     books.append(newBook)
     
     //Image Appending Features (can split into another function to improve aesthetic of code)
     let imgDiv = document.createElement('div')
-    imgDiv.className = "book cover"
+    imgDiv.className = "modal-content"
+    bookContainer.append(imgDiv)
     let newImg = document.createElement('img')
     newImg.className = "book image"
     newImg.setAttribute('src', 'https://1.bp.blogspot.com/-07BtMPlJimo/T2ugvQamV_I/AAAAAAAAKlA/YeBLJcJ5gQU/s1600/booktemplate-generic.png')
-    imgDiv.append(newImg)
-    bookContainer.append(imgDiv)
-    imgDiv.append(bookContent)
-
+    //imgDiv.append(newImg)
+    newBook.append(newImg)
+    imgDiv.append(modalSpan)
   });
 }
 
-
-function toggleContent(element) { 
- element.className === "class1" ? element.className = "class2" : element.className = "class1"
-}
 
 // books.addEventListener('click', toggleContent(class1))
 
